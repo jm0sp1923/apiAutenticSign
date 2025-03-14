5f72b3edf2469e1d6ca44e59de0ddb68613efebc
jm0sp1923 <jm0sp1923@gmail.com>
2025-03-14 10:16:11 -0500

    Unificacion code

--- autenticAutomateSing/src/routes/file.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { consultarArchivoController } from "../controllers/fileController.js";
-import  {validarFile}  from "../middlewares/validarFile.js";
-
-const router = express.Router();
-
-router.post("/consultarArchivo",validarFile, consultarArchivoController);
-
-export default router;
