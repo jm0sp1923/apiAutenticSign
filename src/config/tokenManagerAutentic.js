95768e93ccafd66da37aca1c843853b8553df279
jm0sp1923 <jm0sp1923@gmail.com>
2025-03-14 12:22:07 -0500

    prueba archivo tokenAutentic.json

--- src/config/tokenManagerAutentic.js
+++ src/config/tokenManagerAutentic.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const tokenFilePath = path.join(__dirname, "../constants/tokenAutentic.json");
+const tokenFilePath =  "/tmp/tokenAutentic.json";
 let tokenCache = null; 
 
 async function getValidAutenticToken() {
@@ -41,7 +41,7 @@ async function getValidAutenticToken() {
     };
 
     await fs.writeFile(tokenFilePath, JSON.stringify(newToken, null, 2), "utf-8");
-    console.log("✅ Token guardado correctamente en tokenHubspot.json:", newToken);
+    console.log("✅ Token guardado correctamente en tokenAutentic.json:", newToken);
 
 
     return newToken.token;
