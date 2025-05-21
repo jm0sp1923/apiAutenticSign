980719ba42cbff2055d11e1ceec5d1745cca5fa4
jm0sp1923 <jm0sp1923@gmail.com>
2025-05-21 15:16:38 -0500

    chore: cambio selecion de director comercial recordatorio

--- src/services/rememberMailServices.js
+++ src/services/rememberMailServices.js
@@ -112,11 +112,12 @@ async function enviarCorreoDirector(data) {
   let nameDestinatario = "";
 
 
-  // Determinar destinatario del recordatorio
-  const zonasValidas = ["Antioquia", "Bogotá", "Regiones"];
-if (!zonasValidas.includes(zona)) {
-  throw new Error("Zona no válida.");
-}
+   // Validar la zona y determinar director
+  let zonaBusqueda = zona;
+
+  if (zona !== "Antioquia" && zona !== "Bogotá") {
+    zonaBusqueda = "Regiones"; // Internamente tratamos el resto como "Regiones"
+  }
 
 const director = await Directores.findOne({ zona });
 if (!director || !director.email) {
